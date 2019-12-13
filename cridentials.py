10b5f034ee5f9fcf1364e9d8958332955db92bde
John <johnopana2016@gmail.com>
2019-12-13 16:09:05 +0300

    added description on README.md

--- cridentials.py
+++ cridentials.py
@@ -8,7 +8,7 @@ def create_user(fname,lname,password):
 	new_user = User(fname,lname,password)
 	return new_use
 
-    def save_user(user):
+def save_user(user):
 	'''
 	Function to save a new user account
 	'''
@@ -22,21 +22,21 @@ def verify_user(first_name,password):
 	checking_user = Credential.check_user(first_name,password)
 	return checking_user
 
-    def generate_password():
+def generate_password():
 	'''
 	Function to generate a password automatically
 	'''
 	gen_pass = Credential.generate_password()
 	return gen_pass
 
-    def create_credential(user_name,site_name,account_name,password):
+def create_credential(user_name,site_name,account_name,password):
 	'''
 	Function to create a new credential
 	'''
 	new_credential=Credential(user_name,site_name,account_name,password)
 	return new_credential
 
-    def save_credential(credential):
+def save_credential(credential):
 	'''
 	Function to save a newly created credential
 	'''
@@ -47,13 +47,13 @@ def display_credentials(user_name):
 	Function to display credentials saved by a user
 	'''
 	return Credential.display_credentials(user_name)
-    def copy_credential(site_name):
+def copy_credential(site_name):
 	'''
 	Function to copy a credentials details to the clipboard
 	'''
 	return Credential.copy_credential(site_name)
 
-    def main():
+def main():
 	print(' ')
 	print('Hello! Welcome to Password Locker.')
 	while True:
@@ -63,7 +63,8 @@ def display_credentials(user_name):
 		short_code = input('Enter a choice: ').lower().strip()
 		if short_code == 'ex':
 			break
-            		elif short_code == 'ca':
+
+        elif short_code == 'ca':
 			print("-"*60)
 			print(' ')
 			print('To create a new account:')
@@ -84,7 +85,7 @@ def display_credentials(user_name):
 				print(" ")
 				print(f'Welcome {user_name}. Please choose an option to continue.')
 				print(' ')
-while True:
+                while True:
 					print("-"*60)
 					print('Navigation codes: \n cc-Create a Credential \n dc-Display Credentials \n copy-Copy Password \n ex-Exit')
 					short_code = input('Enter a choice: ').lower().strip()
