83d1a2b22461407cc9d90ffb0492441688174525
John <johnopana2016@gmail.com>
2019-12-13 11:59:44 +0300

    added a method to check if password and name enter are correct

--- user.py
+++ user.py
@@ -33,4 +33,15 @@ class User:
 	# Class Variables
 	credentials_list =[]
 	user_credentials_list = []
+    @classmethod
+	def check_user(cls,first_name,password):
+		'''
+		Method that checks if the name and password entered match entries in the users_list
+		'''
+		current_user = ''
+		for user in User.users_list:
+			if (user.first_name == first_name and user.password == password):
+				current_user = user.first_name
+		return current_user
+
 
