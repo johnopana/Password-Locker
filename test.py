0bea0784deb184cca3143f66a621313a129ff7e4
John <johnopana2016@gmail.com>
2019-12-13 11:26:16 +0300

    add variable for the teardown and test display cridentials

--- test.py
+++ test.py
@@ -80,3 +80,20 @@ def test_save_credentials(self):
 	# 	self.twitter = Credential('Twitter','johnopana','')
 	# 	self.twitter.password = generate_password()
 	# 	self.assertEqual()
+    def tearDown(self):
+		'''
+		Function to clear the credentials list after every test
+		'''
+		Credential.credentials_list = []
+		User.users_list = []
+
+	def test_display_credentials(self):
+		'''
+		Test to check if the display_credentials method, displays the correct credentials.
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Harun','Twitter','johnopana','pswd100')
+		twitter.save_credentials()
+		gmail = Credential('Harun','Gmail','johnopana','pswd200')
+		gmail.save_credentials()
+		self.assertEqual(len(Credential.display_credentials(twitter.user_name)),2)
