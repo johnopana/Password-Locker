5620e5a1c5a667401c36f2e84412d9773364d6b4
John <johnopana2016@gmail.com>
2019-12-13 14:15:13 +0300

    function to creat a new cridential

--- cridentials.py
+++ cridentials.py
@@ -28,3 +28,10 @@ def verify_user(first_name,password):
 	'''
 	gen_pass = Credential.generate_password()
 	return gen_pass
+
+    def create_credential(user_name,site_name,account_name,password):
+	'''
+	Function to create a new credential
+	'''
+	new_credential=Credential(user_name,site_name,account_name,password)
+	return new_credential
