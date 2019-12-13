18323e92d4df68ec9e8e235444ed997292118d01
John <johnopana2016@gmail.com>
2019-12-13 11:50:22 +0300

    added a class to create account and save

--- user.py
+++ user.py
@@ -1,3 +1,11 @@
 import pyperclip
 import random
 import string
+global users_list 
+class User:
+	'''
+	Class to create user accounts and save their information
+	'''
+	# Class Variables
+	# global users_list
+	users_list = []
