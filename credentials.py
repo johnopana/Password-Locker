f512db017a08c1e02270dd96a4cb2e0738a54971
John <johnopana2016@gmail.com>
2019-12-16 14:28:44 +0300

    added function to creat new cridentials

--- credentials.py
+++ credentials.py
@@ -29,3 +29,13 @@ Function to generate a password automatically
 '''
 gen_pass = Credential.generate_password()
 return gen_pass
+
+def create_credential(user_name,site_name,account_name,password):
+'''
+Function to create a new credential
+'''
+new_credential=Credential(user_name,site_name,account_name,password)
+return new_credential
+
+def save_credential(credential):
+'''
