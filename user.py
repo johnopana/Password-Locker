7089e2fe30ad55b892b2b9bd373afb55127329cf
John <johnopana2016@gmail.com>
2019-12-16 13:52:03 +0300

    added a method to define the properties for each user

--- user.py
+++ user.py
@@ -6,4 +6,15 @@ import string
 global users_list
 class User:
 '''
+Class to create user accounts and save their information
+'''
+# Class Variables
+# global users_list
+users_list = []
+def __init__(self,first_name,last_name,password):
+'''
+Method to define the properties for each user object will hold.
+'''
+
+
 
