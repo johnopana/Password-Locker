33fbeea5577b87a6c0d3b3b9e7d5b18170b2797d
John <johnopana2016@gmail.com>
2019-12-16 15:08:38 +0300

    added second user

--- test.py
+++ test.py
@@ -39,3 +39,12 @@ def test_check_user(self):
 '''
 Function to test whether the login in function check_user works as expected
 '''
+self.new_user = User('John','Op\'an\'a','pswd100')
+self.new_user.save_user()
+user2 = User('Ken','Op\'an\'a','pswd100')
+user2.save_user()
+
+for user in User.users_list:
+if user.first_name == user2.first_name and user.password == user2.password:
+current_user = user.first_name
+return current_user
