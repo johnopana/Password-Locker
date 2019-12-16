19b3595d631c6cfa8998abed1383c07ca3338d05
John <johnopana2016@gmail.com>
2019-12-16 14:26:50 +0300

    added  a function to to verify the existence of of the user before creation of cridentials

--- credentials.py
+++ credentials.py
@@ -16,3 +16,16 @@ Function to save a new user account
 User.save_user(user)
 
 
+def verify_user(first_name,password):
+'''
+Function that verifies the existance of the user before creating credentials
+'''
+checking_user = Credential.check_user(first_name,password)
+return checking_user
+
+def generate_password():
+'''
+Function to generate a password automatically
+'''
+gen_pass = Credential.generate_password()
+return gen_pass
