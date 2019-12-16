e5cc15d8a3054240fdd08be670c93b09a94b975f
John <johnopana2016@gmail.com>
2019-12-16 14:09:38 +0300

    created function to to gererate 8 charactor password

--- user.py
+++ user.py
@@ -58,6 +58,22 @@ self.site_name = site_name
 self.account_name = account_name
 self.password = password
 
+def save_credentials(self):
+'''
+Function to save a newly created user instance
+'''
+# global users_list
+Credential.credentials_list.append(self)
+
+def generate_password(size=8, char=string.ascii_uppercase+string.ascii_lowercase+string.digits):
+'''
+Function to generate an 8 character password for a credential
+'''
+gen_pass=''.join(random.choice(char) for _ in range(size))
+return gen_pass
+
+
+
 
 
 
